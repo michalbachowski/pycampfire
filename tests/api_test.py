c11a92431dea298a37fdc5db3b170ec2bb1425fb
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-15 15:53:13 +0200

    Added tests for Api.__init__

--- tests/api_test.py
+++ tests/api_test.py
@@ -6,6 +6,7 @@
 #
 import unittest
 import mox
+import logging
 
 # hack for loading modules
 import _path
@@ -14,11 +15,94 @@ _path.fix()
 ##
 # campfire api modules
 #
-from campfire import api
+from campfire.api import Api
 
 
 class ApiTestCase(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        logging.basicConfig()
+        self.log = logging.getLogger()
+
+    def tearDown(self):
+        pass
+
+    def test_init_require_2_args(self):
+        err = False
+        try:
+            Api()
+        except TypeError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_require_2_args_1(self):
+        err = False
+        try:
+            Api(None)
+        except TypeError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_require_2_args_2(self):
+        err = False
+        try:
+            Api(None, None)
+        except TypeError:
+            err = True
+        except AttributeError:
+            pass
+        self.assertFalse(err)
+
+    def test_init_allows_3_args(self):
+        err = False
+        try:
+            Api(None, None, 10)
+        except TypeError:
+            err = True
+        except AttributeError:
+            pass
+        self.assertFalse(err)
+
+    def test_init_allows_3_args_1(self):
+        err = False
+        try:
+            Api(None, None, None, None)
+        except TypeError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_expects_log_to_be_logging_instance(self):
+        err = False
+        try:
+            Api(None, None)
+        except AttributeError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_expects_cache_size_to_be_int(self):
+        err = False
+        try:
+            Api(self.log, None, 'a')
+        except TypeError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_expects_cache_size_to_be_positive(self):
+        err = False
+        try:
+            Api(self.log, None, -1)
+        except ValueError:
+            err = True
+        self.assertTrue(err)
+
+    def test_init_cache_size_can_be_None(self):
+        err = False
+        try:
+            Api(self.log, None, None)
+        except:
+            err = True
+        self.assertFalse(err)
+
 
 if "__main__" == __name__:
     unittest.main()
