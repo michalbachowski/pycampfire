0cd081f97c165b1d8b04a4425dc6fd19b39a7eea
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-11-27 08:42:01 +0100

    Added log information

--- src/campfire/api.py
+++ src/campfire/api.py
@@ -35,7 +35,7 @@ class Api(object):
         """
         Entry point for new massages
         """
-        self.log.debug('msg=received message; message=%s; user=%s; args=%s', \
+        self.log.info('msg=received message; message=%s; user=%s; args=%s', \
             message, user, args)
         tmp = self._prepare_data(message, user, args)
         self._cache.appendleft(tmp)
@@ -45,21 +45,30 @@ class Api(object):
         """
         Checks authentication for given user
         """
+        self.log.debug('msg=authenticating user; user=%s', user)
         e = self.dispatcher.notify_until(Event(self, 'auth.check', user))
         if not e.processed:
+            self.log.warning('msg=error while authenticating user; user=%s', \
+                user)
             raise AuthError()
+        self.log.debug('msg=user authenticated; user=%s; result=%s', \
+            user, e.return_value)
         return e.return_value
 
     def _prepare_data(self, message, user, args):
         """
         Prepares message data
         """
+        self.log.debug('msg=preparing message; message=%s; user=%s; args=%s', \
+            message, user, args)
         # filter message and prepare final message structure
         e = self.dispatcher.filter(\
             Event(self, 'message.written'), \
             {'id': str(uuid.uuid4()), 'data': {\
                 'message': message, 'from': self._auth_user(user), \
                     'args': args}})
+        self.log.debug('msg=message prepared; message=%s; user=%s; ' + \
+            'args=%s; result=%s', message, user, args, e.return_value)
         return e.return_value
 
     def _filter_output(self, user, message):
@@ -67,14 +76,22 @@ class Api(object):
         Filters messages before the will be send to user
         """
         # filter message
+        self.log.debug('msg=filter output message; message=%s; user=%s', \
+            message, user)
         msg = self.dispatcher.filter(\
             Event(self, 'message.read.filter', {'user': user}), \
             copy.deepcopy(message)).return_value
+        self.log.debug('msg=filtered output message; message=%s; user=%s; ' + \
+            'result=%s', message, user, msg)
         # prevent from returning message to user,
         # that should not read it
+        self.log.debug('msg=checking whether message can be send to user; ' + \
+            'message=%s; user=%s', msg, user)
         e = self.dispatcher.notify_until(\
             Event(self, 'message.read.prevent', {'user': user, \
                 'message': copy.deepcopy(message)}))
+        self.log.debug('msg=checked whether message can be send to user; ' + \
+            'message=%s; user=%s; result=%s', msg, user, e.processed)
         if e.processed:
             return None
         return msg
@@ -83,15 +100,21 @@ class Api(object):
         """
         Sends response to all pollers
         """
+        self.log.debug('msg=sending new message to pollers; ' + \
+            'message=%s', message)
         pollers = copy.copy(self.pollers)
         self.pollers = []
         for (callback, user) in pollers:
             tmp = self._filter_output(user, message)
             # prevent from forgetting connection when message should be not send
             if tmp is None:
+                self.log.debug('msg=reattaching poller; ' + \
+                    'user=%s; callback=%s', user, callback)
                 self.pollers.append((callback, users))
             # send message
             else:
+                self.log.debug('msg=sending message to poller; ' + \
+                    'user=%s; callback=%s', user, callback)
                 self._respond([tmp], callback)
 
     def _respond(self, message, callback):
@@ -104,10 +127,16 @@ class Api(object):
         """
         Attaches poller to list of pollers waiting for message
         """
+        self.log.debug('msg=processing new poller; ' + \
+            'user=%s; cursor=%s', user, cursor)
         tmp = self._fetch_cached_messages(user, cursor)
         if tmp:
+            self.log.debug('msg=found messages newer than given cursor; ' + \
+                'user=%s; cursor=%s; nummsg=%u', user, cursor, len(tmp))
             self._respond(tmp, callback)
             return
+        self.log.debug('msg=attaching new poller; ' + \
+            'user=%s; callback=%s', user, callback)
         self.pollers.append((callback, user))
         return self
 
@@ -115,12 +144,16 @@ class Api(object):
         """
         Removed given item from pollers list
         """
+        self.log.debug('msg=detaching poller; user=%s; callback=%s', item[1], \
+            item[0])
         self.pollers.remove(item)
 
     def detach_poller(self, callback):
         """
         Detaches given poller from list of polles waiting for new messages
         """
+        self.log.debug('msg=detaching pollers for callback; callback=%s', \
+            callback)
         try:
             map(self._do_detach, (i for i in self.pollers if i[0] == callback))
         except ValueError:
@@ -131,9 +164,14 @@ class Api(object):
         """
         Fetches cached messages beginning from given cursor
         """
-        return map(lambda a: a is not None, [self._prepare_output(user, i) \
+        self.log.debug('msg=fetching cached messages; user=%s; cursor=%s', \
+            user, cursor)
+        tmp = map(lambda a: a is not None, [self._prepare_output(user, i) \
             for i in takewhile(partial(self._compare_index, cursor), \
                 self._cache)])
+        self.log.debug('msg=fetched cached messages; user=%s; cursor=%s; ' + \
+            'nummsg=%u', user, cursor, len(tmp))
+        return tmp
 
     def _compare_index(self, cursor, item):
         """
