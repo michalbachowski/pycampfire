8277ee69546dc24d110088dbc7fedc0f659d71ca
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-12-03 15:35:08 +0100

    Fixed tests after changes in Api class

--- tests/api_test.py
+++ tests/api_test.py
@@ -29,6 +29,9 @@ class ApiTestCase(unittest.TestCase):
         self.log = logging.getLogger()
         self.mox = mox.Mox()
         self.listeners = self.mox.CreateMock(Dispatcher)
+        # called when initializing object
+        self.listeners.notify(mox.IsA(Event))
+        self.api = Api(self.log, self.listeners)
 
     def tearDown(self):
         self.mox.UnsetStubs()
@@ -110,57 +113,58 @@ class ApiTestCase(unittest.TestCase):
         self.assertFalse(err)
 
     def test_attach_poller_requires_3_args(self):
-        a = Api(self.log, None)
         err = False
+        self.api.init()
         try:
-            a.attach_poller()
+            self.api.attach_poller()
         except TypeError:
             err = True
         self.assertTrue(err)
 
     def test_attach_poller_requires_3_args_1(self):
-        a = Api(self.log, None)
         err = False
+        self.api.init()
         try:
-            a.attach_poller(None)
+            self.api.attach_poller(None)
         except TypeError:
             err = True
         self.assertTrue(err)
 
     def test_attach_poller_requires_3_args_2(self):
-        a = Api(self.log, None)
         poller = 'abc'
         err = False
+        self.api.init()
         try:
-            a.attach_poller(None, poller)
+            self.api.attach_poller(None, poller)
         except TypeError:
             err = True
         self.assertFalse(err)
-        self.assertTrue(poller in [c for (c, u) in a.pollers if  c == poller])
+        self.assertTrue(poller in [c for (c, u) in self.api.pollers \
+            if  c == poller])
 
     def test_attach_poller_requires_3_args_3(self):
-        a = Api(self.log, None)
         poller = 'abc'
         err = False
+        self.api.init()
         try:
-            a.attach_poller(None, poller, None)
+            self.api.attach_poller(None, poller, None)
         except TypeError:
             err = True
         self.assertFalse(err)
-        self.assertTrue(poller in [c for (c, u) in a.pollers if  c == poller])
+        self.assertTrue(poller in [c for (c, u) in self.api.pollers \
+            if  c == poller])
 
     def test_attach_does_not_check_callback_type(self):
         err = False
-        a = Api(self.log, None)
+        self.api.init()
         try:
-            a.attach_poller(None, 'abc')
+            self.api.attach_poller(None, 'abc')
         except:
             err = True
         self.assertFalse(err)
 
     def test_poller_must_by_callable(self):
         # prepare
-        a = Api(self.log, self.listeners)
         # called when message is received
         e = self.mox.CreateMock(Event)
         e.processed = True
@@ -181,12 +185,13 @@ class ApiTestCase(unittest.TestCase):
         self.listeners.notify_until(mox.IsA(Event)).AndReturn(e2)
         
         self.mox.ReplayAll()
+        self.api.init()
 
         # test
-        a.attach_poller(None, 'a')
+        self.api.attach_poller(None, 'a')
         err = False
         try:
-            a.recv('a', 'b', 'c')
+            self.api.recv('a', 'b', 'c')
         except TypeError:
             err = True
 
@@ -195,59 +200,60 @@ class ApiTestCase(unittest.TestCase):
         self.assertTrue(err)
 
     def test_detach_poller_requires_2_args(self):
-        a = Api(self.log, None)
         err = False
         try:
-            a.detach_poller()
+            self.api.detach_poller()
         except TypeError:
             err = True
         self.assertTrue(err)
 
     def test_detach_poller_requires_2_args_1(self):
-        a = Api(self.log, None)
         poller = 'abc'
         err = False
+        self.api.init()
         try:
-            a.detach_poller(poller)
+            self.api.detach_poller(poller)
         except TypeError:
             err = True
         self.assertFalse(err)
 
     def test_detach_poller_checks_object_id(self):
-        a = Api(self.log, None)
         poller = 'abc'
-        a.attach_poller(None, poller)
-        a.detach_poller('abc')
-        self.assertFalse(poller in [c for (c, u) in a.pollers if  c == poller])
+        self.api.init()
+        self.api.attach_poller(None, poller)
+        self.api.detach_poller('abc')
+        self.assertFalse(poller in [c for (c, u) in self.api.pollers \
+            if  c == poller])
 
     def test_detach_poller_checks_object_id_1(self):
-        a = Api(self.log, None)
         class Poller(object):
             def poller(msg):
                 pass
         poller1 = Poller().poller
         poller2 = Poller().poller
-        a.attach_poller(None, poller1)
-        a.detach_poller(poller2)
-        self.assertTrue(poller1 in [c for (c, u) in a.pollers if  c == poller1])
+        self.api.init()
+        self.api.attach_poller(None, poller1)
+        self.api.detach_poller(poller2)
+        self.assertTrue(poller1 in [c for (c, u) in self.api.pollers \
+            if  c == poller1])
 
     def test_detach_poller_checks_object_id_2(self):
-        a = Api(self.log, None)
         class Poller(object):
             def poller(msg):
                 pass
         poller1 = Poller().poller
         poller2 = Poller().poller
-        a.attach_poller(None, poller1)
-        a.attach_poller(None, poller2)
-        a.detach_poller(poller2)
-        self.assertTrue(poller1 in [c for (c, u) in a.pollers if  c == poller1])
-        self.assertFalse(poller2 in [c for (c, u) in a.pollers \
+        self.api.init()
+        self.api.attach_poller(None, poller1)
+        self.api.attach_poller(None, poller2)
+        self.api.detach_poller(poller2)
+        self.assertTrue(poller1 in [c for (c, u) in self.api.pollers \
+            if  c == poller1])
+        self.assertFalse(poller2 in [c for (c, u) in self.api.pollers \
             if  c == poller2])
 
     def test_poller_is_called_on_recv(self):
         # prepare
-        a = Api(self.log, self.listeners)
         # called when message is received
         e = self.mox.CreateMock(Event)
         e.processed = True
@@ -271,12 +277,13 @@ class ApiTestCase(unittest.TestCase):
         p(mox.IsA(list))
         
         self.mox.ReplayAll()
+        self.api.init()
 
         # test
         err = False
-        a.attach_poller(None, p)
+        self.api.attach_poller(None, p)
         try:
-            a.recv('a', 'b', 'c')
+            self.api.recv('a', 'b', 'c')
         except TypeError:
             err = True
 
@@ -286,9 +293,8 @@ class ApiTestCase(unittest.TestCase):
 
     def test_run(self):
         # prepare
-        a = Api(self.log, self.listeners)
         pollers = []
-
+        
         def side_effect1(e, a, b):
             b['tmp'] = a['user']
             e.return_value = b
@@ -304,26 +310,30 @@ class ApiTestCase(unittest.TestCase):
         self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
             partial(side_effect, e)).AndReturn(e)
 
-        # called when notifying pollers
-        for i in xrange(1, 3):
+        # called when attaching and notifying pollers
+        for i in xrange(0, 2):
             e2 = self.mox.CreateMock(Event)
             e2.processed = False
             e2.return_value = None
-    
+        
+            # called when sending response to poller
             self.listeners.filter(mox.IsA(Event), mox.IsA(dict)\
                 ).WithSideEffects(partial(side_effect1, e2)).AndReturn(e2)
             self.listeners.notify_until(mox.IsA(Event)).AndReturn(e2)
             
-            # called when sending response to poller
             p = self.mox.CreateMockAnything()
             p([{'tmp': i, 'data': {'message': 'a', 'args': 'c', 'from': None}, \
                 'id': mox.IsA(str)}])
-            a.attach_poller(i, p)
+            pollers.append(p)
 
         self.mox.ReplayAll()
+        
+        self.api.init()
+        for (i, p) in enumerate(pollers):
+            self.api.attach_poller(i, p)
 
         # test
-        a.recv('a', 'b', 'c')
+        self.api.recv('a', 'b', 'c')
 
         # verify
         self.mox.VerifyAll()
@@ -338,6 +348,7 @@ class ApiTestCase(unittest.TestCase):
         self.listeners.notify(mox.IsA(Event)).WithSideEffects(side_effect)
 
         self.mox.ReplayAll()
+        self.api.init()
 
         # test
         a = Api(self.log, self.listeners)
@@ -349,8 +360,7 @@ class ApiTestCase(unittest.TestCase):
 
     def test_shutdown_sends_event_and_closes_connections(self):
         # prepare
-        a = Api(self.log, self.listeners)
-        
+        pollers = []
         out = {'route': None}
         def side_effect(e):
             out['route'] = e.name
@@ -358,15 +368,19 @@ class ApiTestCase(unittest.TestCase):
         self.listeners.notify(mox.IsA(Event)).WithSideEffects(side_effect)
         
         # called when shutting down pollers
-        for i in xrange(1, 3):
+        for i in xrange(0, 2):
             p = self.mox.CreateMockAnything()
             p(mox.IsA(list))
-            a.attach_poller(i, p)
+            pollers.append(p)
 
         self.mox.ReplayAll()
+        self.api.init()
+        
+        for (i, p) in enumerate(pollers):
+            self.api.attach_poller(i, p)
 
         # test
-        a.shutdown()
+        self.api.shutdown()
 
         # verify
         self.mox.VerifyAll()
