67e2f45843eb1958f8322a8f97914ea323d4ae1d
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-01-15 13:40:13 +0100

    Added some more tests to better cover code

--- tests/api_test.py
+++ tests/api_test.py
@@ -4,6 +4,7 @@
 ##
 # python standard library
 #
+import time
 import unittest
 import mox
 import logging
@@ -20,7 +21,7 @@ from event import Dispatcher, Event
 # campfire api modules
 #
 from campfire.api import Api, ChatReinitializationForbiddenError, \
-    UninitializedChatError
+    UninitializedChatError, AuthError
 
 
 class ApiTestCase(unittest.TestCase):
@@ -155,7 +156,223 @@ class ApiTestCase(unittest.TestCase):
         self.assertTrue(poller in [c for (c, u) in self.api.pollers \
             if  c == poller])
 
-    def test_attach_does_not_check_callback_type(self):
+    def test_attach_poller_doest_not_attach_poller_when_any_messages_are_found(self):
+        # prepare
+        pollers = []
+        
+        def side_effect2(a, b):
+            e1.return_value = b
+        
+        def side_effect1(e, a, b):
+            b['tmp'] = a['user']
+            e.return_value = b
+
+        def side_effect(e, a, b):
+            e.return_value = b
+
+        # called when message is received
+        e = self.mox.CreateMock(Event)
+        e.processed = True
+        e.return_value = None
+        e.__getitem__('response').AndReturn({})
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            partial(side_effect, e)).AndReturn(e)
+
+        # called when preparing response to request
+        e1 = self.mox.CreateMock(Event)
+        e1.processed = True
+        e1.return_value = None
+        
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            side_effect2).AndReturn(e1)
+
+        # called when attaching and notifying pollers
+        for i in xrange(0, 2):
+            e2 = self.mox.CreateMock(Event)
+            e2.processed = False
+            e2.return_value = None
+        
+            # called when sending response to poller
+            self.listeners.notify_until(mox.IsA(Event)).AndReturn(e2)
+            self.listeners.filter(mox.IsA(Event), mox.IsA(dict)\
+                ).WithSideEffects(partial(side_effect1, e2)).AndReturn(e2)
+            
+            p = self.mox.CreateMockAnything()
+            p([{'tmp': i, 'text': 'a', 'args': 'c', 'date': mox.IsA(int), \
+                'from': 'b', 'id': mox.IsA(str)}])
+            pollers.append(p)
+
+        self.mox.ReplayAll()
+        
+        self.api.init()
+
+        # test
+        self.api.recv('a', 'b', 'c')
+
+        for (i, p) in enumerate(pollers):
+            self.api.attach_poller(i, p)
+
+        # verify
+        self.mox.VerifyAll()
+
+    def test_poller_will_not_receive_messages_that_should_not_be_returned(self):
+        def side_effect2(a, b):
+            e1.return_value = b
+
+        def side_effect1(e, a, b):
+            e.return_value = b
+
+        def side_effect(e, a, b):
+            e.return_value = b
+
+        # called when message is received
+        e = self.mox.CreateMock(Event)
+        e.processed = True
+        e.return_value = None
+        e.__getitem__('response').AndReturn({})
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            partial(side_effect, e)).AndReturn(e)
+
+        # called when attaching and notifying pollers
+        e2 = self.mox.CreateMock(Event)
+        e2.processed = True
+        e2.return_value = None
+    
+        # called when sending response to poller
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).AndReturn(e2)
+        
+        p = self.mox.CreateMockAnything()
+        i = 'usr'
+
+        # called when preparing response to request
+        e1 = self.mox.CreateMock(Event)
+        e1.processed = True
+        e1.return_value = None
+        
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e1)
+
+        self.mox.ReplayAll()
+        
+        self.api.init()
+
+        # test
+        self.api.recv('a', 'b', 'c')
+        self.api.attach_poller(i, p)
+        
+        # verify
+        self.mox.VerifyAll()
+    
+    def test_poller_will_not_receive_messages_that_are_too_old(self):
+        def side_effect2(a, b):
+            e1.return_value = b
+
+        def side_effect1(e, a, b):
+            e.return_value = b
+
+        def side_effect(e, a, b):
+            b['date'] = time.time() - 365 * 24 * 60 * 60 # year ago
+            e.return_value = b
+
+        # called when message is received
+        e = self.mox.CreateMock(Event)
+        e.processed = True
+        e.return_value = None
+        e.__getitem__('response').AndReturn({})
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            partial(side_effect, e)).AndReturn(e)
+
+        # called when attaching and notifying pollers
+        e2 = self.mox.CreateMock(Event)
+        e2.processed = True
+        e2.return_value = None
+    
+        # called when sending response to poller
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).AndReturn(e2)
+        
+        p = self.mox.CreateMockAnything()
+        i = 'usr'
+
+        self.mox.ReplayAll()
+        
+        self.api.init()
+
+        # test
+        self.api.recv('a', 'b', 'c')
+        self.api.attach_poller(i, p)
+        
+        # verify
+        self.mox.VerifyAll()
+
+    def test_attach_poller_attaches_poller_when_cursor_indicates_last_message(self):
+        # prepare
+        pollers = []
+        msgs = []
+        
+        def side_effect3(m):
+            msgs.extend(m)
+        
+        def side_effect2(a, b):
+            e1.return_value = b
+
+        def side_effect1(e, a, b):
+            b['tmp'] = a['user']
+            e.return_value = b
+
+        def side_effect(e, a, b):
+            e.return_value = b
+
+        # called when message is received
+        e = self.mox.CreateMock(Event)
+        e.processed = True
+        e.return_value = None
+        e.__getitem__('response').AndReturn({})
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            partial(side_effect, e)).AndReturn(e)
+
+        # called when attaching and notifying pollers
+        e2 = self.mox.CreateMock(Event)
+        e2.processed = False
+        e2.return_value = None
+    
+        # called when sending response to poller
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e2)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)\
+            ).WithSideEffects(partial(side_effect1, e2)).AndReturn(e2)
+        
+        p = self.mox.CreateMockAnything()
+        i = 'usr'
+        p([{'tmp': i, 'text': 'a', 'args': 'c', 'date': mox.IsA(int), \
+            'from': 'b', 'id': mox.IsA(str)}]).WithSideEffects(side_effect3)
+        pollers.append(p)
+
+        p1 = self.mox.CreateMockAnything()
+
+        # called when preparing response to request
+        e1 = self.mox.CreateMock(Event)
+        e1.processed = True
+        e1.return_value = None
+        
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            side_effect2).AndReturn(e1)
+
+        self.mox.ReplayAll()
+        
+        self.api.init()
+        self.api.attach_poller(i, p)
+
+        # test
+        self.api.recv('a', 'b', 'c')
+        
+        self.api.attach_poller('usr', p1, msgs[0]['id'])
+
+        # verify
+        self.mox.VerifyAll()
+
+    def test_attach_poller_does_not_check_callback_type(self):
         err = False
         self.api.init()
         try:
@@ -163,6 +380,14 @@ class ApiTestCase(unittest.TestCase):
         except:
             err = True
         self.assertFalse(err)
+    
+    def test_attach_poller_raises_exception_when_chat_is_not_initialized(self):
+        err = False
+        try:
+            self.api.attach_poller(None, None, None)
+        except UninitializedChatError:
+            err = True
+        self.assertTrue(err)
 
     def test_poller_must_by_callable(self):
         # prepare
@@ -253,6 +478,22 @@ class ApiTestCase(unittest.TestCase):
             if  c == poller1])
         self.assertFalse(poller2 in [c for (c, u) in self.api.pollers \
             if  c == poller2])
+    
+    def test_detach_poller_raises_exception_when_chat_is_not_initialized(self):
+        err = False
+        try:
+            self.api.detach_poller(None)
+        except UninitializedChatError:
+            err = True
+        self.assertTrue(err)
+    
+    def test_recv_raises_exception_when_chat_is_not_initialized(self):
+        err = False
+        try:
+            self.api.recv(None, None, None)
+        except UninitializedChatError:
+            err = True
+        self.assertTrue(err)
 
     def test_poller_is_called_on_recv(self):
         # prepare
@@ -292,6 +533,64 @@ class ApiTestCase(unittest.TestCase):
         # test
         err = False
         self.api.attach_poller(None, p)
+        try:
+            self.api.recv('a', 'b', 'c')
+        except TypeError:
+            err = True
+
+        # verify
+        self.mox.VerifyAll()
+        self.assertFalse(err)
+    
+    def test_recv_raises_exception_when_auth_chat_event_was_not_processed(self):
+        e = self.mox.CreateMock(Event)
+        e.processed = False
+        e.return_value = None
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.mox.ReplayAll()
+
+        self.api.init()
+        try:
+            self.api.recv(None, None, None)
+        except AuthError:
+            err = True
+        
+        self.mox.VerifyAll()
+        self.assertTrue(err)
+    
+    def test_when_event_read_prevent_is_processed_message_will_be_ignored(self):
+        # prepare
+        # called when message is received (recv())
+        e = self.mox.CreateMock(Event)
+        e.processed = True
+        e.return_value = None
+        e.__getitem__('response').AndReturn({})
+        def side_effect(a, b):
+            e.return_value = b
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e)
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            side_effect).AndReturn(e)
+        # called when notifying pollers (filter output)
+        e2 = self.mox.CreateMock(Event)
+        e2.processed = True
+        e2.return_value = None
+        def side_effect2(a, b):
+            e2.return_value = b
+        self.listeners.notify_until(mox.IsA(Event)).AndReturn(e2)
+        # called before sending response to sender (_prepare_response())
+        e1 = self.mox.CreateMock(Event)
+        e1.processed = True
+        e1.return_value = None
+        def side_effect1(a, b):
+            e1.return_value = b
+        self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
+            side_effect1).AndReturn(e1)
+        self.mox.ReplayAll()
+        self.api.init()
+
+        # test
+        err = False
+        self.api.attach_poller(None, lambda f: f)
         try:
             self.api.recv('a', 'b', 'c')
         except TypeError:
@@ -340,10 +639,10 @@ class ApiTestCase(unittest.TestCase):
         e1 = self.mox.CreateMock(Event)
         e1.processed = True
         e1.return_value = None
-        def side_effect1(a, b):
+        def side_effect2(a, b):
             e1.return_value = b
         self.listeners.filter(mox.IsA(Event), mox.IsA(dict)).WithSideEffects(\
-            side_effect1).AndReturn(e1)
+            side_effect2).AndReturn(e1)
 
         self.mox.ReplayAll()
         
@@ -421,6 +720,7 @@ class ApiTestCase(unittest.TestCase):
         except UninitializedChatError:
             err = True
         self.assertTrue(err)
+
 
 if "__main__" == __name__:
     unittest.main()
